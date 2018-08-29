955e5c202e9eadf58e258936d1372a5eb5f6ef63
imjamin <imjamin@qq.com>
2018-08-29 18:45:53 +0800

    heap初始

---
+++ src/heap/MaxHeap.java
@@ -0,0 +1,38 @@
+package heap;
+
+import array.Array;
+
+public class MaxHeap<E extends Comparable> {
+    private Array<E> data;
+
+    public void MaxHeap(int cap){
+        data=new Array<>(cap);
+    }
+
+    // 返回堆中的元素个数
+    public int size(){
+        return data.getSize();
+    }
+
+    // 返回一个布尔值, 表示堆中是否为空
+    public boolean isEmpty(){
+        return data.isEmpty();
+    }
+
+    // 返回完全二叉树的数组表示中，一个索引所表示的元素的父亲节点的索引
+    private int parent(int index){
+        if (index<=0)
+            throw new IllegalArgumentException("index-0 doesn't have parent.");
+        return --index/2;
+    }
+    // 返回完全二叉树的数组表示中，一个索引所表示的元素的左孩子节点的索引
+    private int leftChild(int index){
+        return index * 2 + 1;
+    }
+    // 返回完全二叉树的数组表示中，一个索引所表示的元素的右孩子节点的索引
+    private int rightChild(int index){
+        return index * 2 + 2;
+    }
+    //add & sift up
+    public void add()
+}
