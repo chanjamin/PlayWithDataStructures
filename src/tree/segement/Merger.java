7f3ad80262274c4bd236f165df62b2a294ca9fec
imjamin <imjamin@qq.com>
2018-11-06 11:00:50 +0800

    线段树查询

--- src/tree/segement/Merger.java
+++ src/tree/segement/Merger.java
@@ -1,5 +1,5 @@
 package tree.segement;
 
 public interface Merger<E> {
-    E merge(E a,E b);
+    E    merge(E a,E b);
 }
--- src/tree/segement/TestSegTree.java
+++ src/tree/segement/TestSegTree.java
@@ -5,5 +5,9 @@ public class TestSegTree {
         Integer[] nums = {-2, 0, 3, -5, 2, -1};
         SegmentTree<Integer> segmentTree = new SegmentTree<>(nums, (Merger<Integer>) (a, b) -> a + b);
         System.out.println(segmentTree);
+
+        System.out.println(segmentTree.query(0,2));
+        System.out.println(segmentTree.query(2,3));
+        System.out.println(segmentTree.query(0,5));
     }
 }
